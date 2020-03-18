82316ef9f6b7a9ee6ec4d3fa89139b0bac8afc46
cendert <github@christiaanendert.nl>
2020-03-18 09:05:59 +0100

    initial post-private

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      new and improved
+      new and improved asdf
     </div>
   );
 }
